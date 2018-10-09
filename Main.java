8456d954700016806d1e12eb74fceee6fa444df0
lazerbird <43896390+lazerbird@users.noreply.github.com>
2018-10-09 11:12:39 -0700

    10/9/18

--- Main.java
+++ Main.java
@@ -31,24 +31,31 @@ class Main
          */
         System.out.println("*** PRINTING ar1 *");
         int[] ar1 = new int[n];
-      /*  
-        for (int i=0; i<ar1.length;i++) {
-        ar1[i]=i;
-    }
-        for (int i=0; i<ar1.length; i++) 
-        System.out.println("ar[" + i + "] = " +ar1[i]);
-    */
+        
+        for (int i=0; i<n;i++) 
+            ar1[i]=i;
+    
+        for (int i=0; i<n; i++) 
+            System.out.println("ar[" + i + "] = " +ar1[i]);
+    
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
-        int[] ar2=new int[n];
-        ar2=ar1;
-        System.out.println(ar2);
+        System.out.println("*** PRINTING ar2 *");
+        int[] ar2=ar1.clone();
+        ar1[5]=100;
+        
+        for (int i=0; i<n; i++) 
+            ar2[i] = i;
+        
+        for (int i=0; i<n; i++) 
+            System.out.println("ar2["+i+"] = "+ar2[i]);
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
-        
+        for (int i=0; i<n; i++) 
+            System.out.println("ar1["+i+"] = "+ar1[i]+1);
 
         
         
